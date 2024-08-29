39621bfdb2d353047ff18ca9d07e210ad44836ea
Raigis <alerish015@gmail.com>
2024-08-29 19:17:31 +0300

    Complete second task

--- SecondTask/main2.cpp
+++ SecondTask/main2.cpp
@@ -14,17 +14,17 @@
 
 #include <iostream>
 
-void print(char field[][6]){
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++){
+void print(char field[][8]){
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++){
             std::cout << field[i][j];
         }
         std::cout << std::endl;
     }
 }
 
-bool win (bool BF[][3], std::string player) {
-    if ((BF[0][0] && BF[1][0] && BF[2][0])
+bool win (bool BF[][3]) {
+    if (       (BF[0][0] && BF[1][0] && BF[2][0])
             || (BF[0][1] && BF[1][1] && BF[2][1])
             || (BF[0][2] && BF[1][2] && BF[2][2])
             || (BF[0][0] && BF[0][1] && BF[0][2])
@@ -32,35 +32,77 @@ bool win (bool BF[][3], std::string player) {
             || (BF[2][0] && BF[2][1] && BF[2][2])
             || (BF[0][0] && BF[1][1] && BF[2][2])
             || (BF[0][2] && BF[1][1] && BF[2][0])) {
-        std::cout << player << " win!";
         return true;
     }
     return false;
 }
 
-bool draw (bool BF[][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j =0; j < 3; j++){
-            if(!BF[i][j]) return false;
-        }
+void change_field(bool BFP[][3], bool BF[][3], char CF[][8], int xy[], int x, int y, char mark){
+    BFP[x][y] = true;
+    BF[x][y] = true;
+    CF[xy[x]][xy[y]] = mark;
+}
+
+bool is_correct(bool BF[][3], int x, int y) {
+    if(BF[x][y] || x < 0 || x > 2 || y < 0 || y > 2){
+        std::cout << "Incorrect input. Please, try again.\n";
+        return true;
     }
-    std::cout << "Draw!";
-    return true;
+    return false;
+}
+
+void play(bool BFP[][3], bool BF[][3],char CF[][8], int xy[], int id){
+    std::string player = (id == 0 ? "First player" : "Second player");
+    char mark = (id == 0 ? 'X' : 'O');
+    char x,y;
+    do{
+        std::cout << player << " turn!\nEnter the coordinates (Left Up):\n";
+        std::cin >> x >> y;
+    } while (is_correct(BF, (x - '0') - 1, (y - '0') - 1));
+    change_field(BFP, BF, CF, xy, (x - '0') - 1, (y - '0') - 1, mark);
 }
 
 int main() {
-    char charField[6][6] {{' ', '1', ' ', '2', ' ', '3'},
-                          {'1', ' ', '|', ' ', '|', ' '},
-                          {' ', '-', '+', '-', '+', '-'},
-                          {'2', ' ', '|', ' ', '|', ' '},
-                          {' ', '-', '+', '-', '+', '-'},
-                          {'3', ' ', '|', ' ', '|', ' '}};
+    char charField[8][8] {{' ', ' ', '1', ' ', '2', ' ', '3', ' '},
+                          {' ', ' ', '-', '-', '-', '-', '-', ' '},
+                          {'1', '|', ' ', '|', ' ', '|', ' ', '|'},
+                          {' ', '|', '-', '+', '-', '+', '-', '|'},
+                          {'2', '|', ' ', '|', ' ', '|', ' ', '|'},
+                          {' ', '|', '-', '+', '-', '+', '-', '|'},
+                          {'3', '|', ' ', '|', ' ', '|', ' ', '|'},
+                          {' ', ' ', '-', '-', '-', '-', '-', ' '}};
+
     bool boolField[3][3] {{false, false, false},
                           {false, false, false},
-                          {false, false, false}};
-    int coordinates[3] {1, 3, 5};
-    std::string player = "First player";
-    while(!win(boolField, player) && !draw(boolField)){
+                          {false, false, false}}, 
+    boolFieldFP[3][3] {{false, false, false},
+                       {false, false, false},
+                       {false, false, false}}, 
+    boolFieldSP[3][3] {{false, false, false},
+                       {false, false, false},
+                       {false, false, false}};
+
+    int coordinates[3] {2, 4, 6};
+    int player;
+    int count = 0;
+    while(true){
         print(charField);
+        player = count % 2;
+        play((player == 0 ? boolFieldFP : boolFieldSP), boolField, charField, coordinates, player);
+        if (count >= 4) {
+            if (win(player == 0 ? boolFieldFP : boolFieldSP)) {
+                std::cout << "\033[1J\033[2;2H";
+                print(charField);
+                std::cout << (player == 0 ? "First player" : "Second player") << " win!";
+                return 0;
+            } else if (count == 8) {
+                std::cout << "\033[1J\033[2;2H";
+                print(charField);
+                std::cout << "Draw!";
+                return 0;
+            }
+        }
+        count++;
+        std::cout << "\033[1J\033[2;2H";
     }
 }
